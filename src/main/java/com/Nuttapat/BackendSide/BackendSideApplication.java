dd85ad44ef52d51d861833a6ef4e3e1282be5cc3
Nuttapat Pothavichai <nuttapatp@outlook.co.th>
2023-11-28 13:24:34 +0700

    third commit

--- src/main/java/com/Nuttapat/BackendSide/BackendSideApplication.java
+++ src/main/java/com/Nuttapat/BackendSide/BackendSideApplication.java
@@ -1,13 +1,18 @@
 package com.Nuttapat.BackendSide;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendSideApplication {
+	private static final Logger logger = LoggerFactory.getLogger(BackendSideApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSideApplication.class, args);
-	}
 
+		logger.info("SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		logger.info("SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+	}
 }
